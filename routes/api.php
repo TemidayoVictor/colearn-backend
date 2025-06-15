758c114750d31a4f51a426364161ede585014a9f
Temidayo Victor <temidayovictor1@gmail.com>
2025-06-15 22:22:27 +0100

    onboarding

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Api\V1\Auth\SignUpController;
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 
 Route::prefix('v1')->group(function () {
-
-    // route to verify if user is logged in
-    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-        return $request->user();
-    });
-
     Route::post('/createAccount', [SignUpController::class, 'createAccount']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Api\V1\Auth\SignUpController;
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// route to verify if user is logged in
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
 
 Route::post('/login', [AuthController::class, 'login']);
